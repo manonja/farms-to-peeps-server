cc284577f28009697e2cccab6a54a832749bfc09
Manon Jacquin <manon.jacquin@gmail.com>
2019-06-07 11:27:28 +0100

    user controller done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,22 @@
 class UsersController < ApplicationController
+    
+    
+    
     def index
-        farmers = Farmer.all
-        render json: farmers
+        users = User.all
+        render json: users
       end
     
       def new
-        farmer = Farmer.new
+        user = User.new
       end
     
       def show
-        farmer = Farmer.find_by(id: params[:id])
-        if farmer
-          render json: farmer
+        user = User.find_by(id: params[:id])
+        if user
+          render json: user
         else
-          render json: {error: "This farmer does not exist?"}, status: 404
+          render json: {error: "This user does not exist?"}, status: 404
         end
       end
     
@@ -21,17 +24,17 @@ class UsersController < ApplicationController
         username = params[:email]
         password = params[:password_digest]
     
-        farmer = Farmer.find_or_create_by(email)
+        user = User.find_or_create_by(email)
     
         if user
-          render json: farmer
+          render json: user
         else
           render json: {error: "Erorr creating farmer"}, status: 400
         end
       end
     
       private
-      def farmer_params
-        params.require(:farmer).permit(:id, :name, :farm, :email, :phone, :password)
+      def user_params
+        params.require(:user).permit(:id, :username, :isAdmin, :password)
       end
 end
