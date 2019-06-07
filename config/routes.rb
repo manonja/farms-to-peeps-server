19bd99e7794b389e4a5e439d0a46b895d75e20f9
Manon Jacquin <manon.jacquin@gmail.com>
2019-06-07 11:45:39 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :create, :new]
+  resources :customers, only: [:index, :show, :create, :new]
+  resources :farmers, only: [:index, :new, :create, :show]
+  resources :products, only: [:index, :new, :create, :show, :destroy]
+
+  post '/signin', to: 'users#signin'
+  post '/signup', to: 'users#signup'
+  get '/validate', to: 'users#validate'
+
+
 end
