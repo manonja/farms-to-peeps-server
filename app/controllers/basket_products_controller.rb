fdeef2c9aefde07d0ef962ea4ebff94c55bad0d9
Manon Jacquin <manon.jacquin@gmail.com>
2019-06-17 14:41:44 +0100

    Changes variable names to satisfy Rubocop

--- app/controllers/basket_products_controller.rb
+++ app/controllers/basket_products_controller.rb
@@ -1,47 +1,48 @@
+# frozen_string_literal: true
+
+# The BasketProductsController class manages all the API routes related to basket_products
 class BasketProductsController < ApplicationController
-    
   def remove
-    basketProduct = BasketProduct.find_by(product_id: params[:product_id], basket_id: params[:basket_id])
-    if basketProduct
-      basketProduct.destroy
-      render json: {message: "Relationship destroyed"}
+    basket_product = BasketProduct.find_by(product_id: params[:product_id], basket_id: params[:basket_id])
+    if basket_product
+      basket_product.destroy
+      render json: {message: 'Relationship destroyed'}
     else
-      render json: {error: "Could not destroy"}, status: 404
+      render json: {error: 'Could not destroy'}, status: 404
     end
   end
 
   def index
-    basketProducts = BasketProduct.all
-    render json: basketProducts
+    render json: BasketProduct.all
   end
-    
+
   def new
-    basketProduct = BasketProduct.new
+    BasketProduct.new
   end
 
   def show
-    basketProduct = BasketProduct.find_by(id: params[:id])
-    if basketProduct
-      render json: basketProduct
+    basket_product = BasketProduct.find_by(id: params[:id])
+    if basket_product
+      render json: basket_product
     else
-      render json: {error: "This basketProduct does not exist?"}, status: 404
+      render json: {error: 'This basketProduct does not exist?'}, status: 404
     end
   end
 
   def create    
-    basketProduct = BasketProduct.create(product_id: params[:product_id], basket_id: params[:basket_id])
+    basket_product = BasketProduct.create(product_id: params[:product_id], basket_id: params[:basket_id])
 
-    if basketProduct
-      render json: basketProduct
+    if basket_product
+      render json: basket_product
     else
-      render json: {error: "Erorr creating product"}, status: 400
+      render json: {error: 'Erorr creating product'}, status: 400
     end
   end
 
   def destroy
-    basketProduct = BasketProduct.find_by(basketProduct_params)
-    if basketProduct
-      basketProduct.destroy
+    basket_product = BasketProduct.find_by(basket_product_params)
+    if basket_product
+      basket_product.destroy
       render json: {message: "Relationship destroyed"}
     else
       render json: {error: "Could not destroy"}, status: 404
@@ -49,7 +50,7 @@ class BasketProductsController < ApplicationController
   end
 
   private
-  def basketProduct_params
+  def basket_product_params
     params.require(:basketProduct).permit(:product_id, :basket_id)
   end
 end
